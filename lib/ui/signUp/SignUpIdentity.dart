1ba65a6f09dbc1310894238d2adfae763a074112
Min Xuan <63584286+minxuan1509@users.noreply.github.com>
2020-10-06 22:26:27 +0800

    不完善的signup identity page

---
+++ lib/ui/signUp/SignUpIdentity.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class SignUpIdentity extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: new Scaffold(
+        appBar: AppBar(
+          title: Text("Choose your identity"),
+          textTheme: Theme.of(context).textTheme.apply(
+                bodyColor: Colors.black,
+                displayColor: Colors.black,
+              ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+        ),
+        backgroundColor: Colors.white10,
+        body: Padding(
+          padding: const EdgeInsets.all(65.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(width: 20.0),
+              FlatButton(
+                onPressed: () {
+                  //Navigator.popAndPushNamed(context, "/SignUpScreen");
+                  print('I got clicked');
+                },
+                child: Image.asset('assets/Teacher.png'),
+              ),
+              SizedBox(width: 50.0),
+              FlatButton(
+                onPressed: () {
+                  print('I got clicked');
+                  //Navigator.popAndPushNamed(context, "/SignUpScreen");
+                },
+                child: Image.asset('assets/Student.png'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
