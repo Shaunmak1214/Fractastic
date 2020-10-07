244d1f9e5402eb75a2d8f4c42b69100fbd2f1bf3
Min Xuan <63584286+minxuan1509@users.noreply.github.com>
2020-10-07 22:31:55 +0800

    Gme Link URL

--- lib/ui/chapter/chapter1/game1.dart
+++ lib/ui/chapter/chapter1/game1.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Chap1game1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: _launchURL,
+          child: Text('Play Game !'),
+        ),
+      ),
+    );
+  }
+}
+
+_launchURL() async {
+  {
+    const url = 'https://www.mathplayground.com/fraction_forest_part1.html';
+    if (await canLaunch(url)) {
+      print('loading url');
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
