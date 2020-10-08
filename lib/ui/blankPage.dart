fe3bbc19f51d38505596142b75031e21bf0f5903
Chiaxuan Lee <70343352+chiaxuan00@users.noreply.github.com>
2020-10-09 07:13:30 +0800

    fix error

--- lib/ui/blankPage.dart
+++ lib/ui/blankPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fractastic/constants.dart' as Constants;
 
 class BlankPage extends StatelessWidget {
+  String _currentClassCode;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,13 +24,86 @@ class BlankPage extends StatelessWidget {
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(Constants.COLOR_ACCENT),
-        child: Text('Back'),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-      ),
+      floatingActionButton: Container(
+          height: 60.0,
+          width: 60.0,
+          child: FittedBox(
+            child: FloatingActionButton(
+                child: Icon(Icons.add),
+                backgroundColor: Color(Constants.COLOR_ACCENT),
+                onPressed: () {
+                  _asyncJoinClassDialog(context);
+                }),
+          ),
+    );
+  }
+
+  Future _asyncJoinClassDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      barrierDismissible:
+          true, // dialog is dismissible with a tap on the barrier
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Jpin Class'),
+          content: Stack(
+            overflow: Overflow.visible,
+            children: <Widget>[
+              Positioned(
+                right: -40.0,
+                top: -80.0,
+                child: InkResponse(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: CircleAvatar(
+                    child: Icon(Icons.close),
+                    backgroundColor: Colors.red,
+                  ),
+                ),
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Class code',
+                          contentPadding:
+                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                        ),
+                        validator: validateClassCode,
+                        onChanged: (val) =>
+                            setState(() => _currentClassCode = val),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RaisedButton(
+                        child: Text("Join"),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() {
+                              user.classCode =
+                                  _currentClassCode ?? user.classCode;
+                            });
+                            await _fireStoreUtils.updateCurrentUser(
+                                user, context);
+                            Navigator.pop(context);
+                          }
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/ui/student/studentScreen.dart
+++ lib/ui/student/studentScreen.dart
@@ -44,7 +44,6 @@ class _StudentScreenState extends State<StudentScreen> {
   String _currentLastName;
   String _currentPassword;
   String _currentGuardianEmail;
-  String _currentClassCode;
 
   int currentIndex = 0;
 
@@ -176,19 +175,6 @@ class _StudentScreenState extends State<StudentScreen> {
         body: _widgetOptions.elementAt(currentIndex),
 
         //only homescreen got join class
-
-        floatingActionButton: Container(
-          height: 60.0,
-          width: 60.0,
-          child: FittedBox(
-            child: FloatingActionButton(
-                child: Icon(Icons.add),
-                backgroundColor: Color(Constants.COLOR_ACCENT),
-                onPressed: () {
-                  _asyncJoinClassDialog(context);
-                }),
-          ),
-        ),
       ),
     );
   }
@@ -223,75 +209,6 @@ class _StudentScreenState extends State<StudentScreen> {
   //     },
   //   );
   // }
-
-  Future _asyncJoinClassDialog(BuildContext context) async {
-    return showDialog(
-      context: context,
-      barrierDismissible:
-          true, // dialog is dismissible with a tap on the barrier
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Jpin Class'),
-          content: Stack(
-            overflow: Overflow.visible,
-            children: <Widget>[
-              Positioned(
-                right: -40.0,
-                top: -80.0,
-                child: InkResponse(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: CircleAvatar(
-                    child: Icon(Icons.close),
-                    backgroundColor: Colors.red,
-                  ),
-                ),
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Class code',
-                          contentPadding:
-                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                        ),
-                        validator: validateClassCode,
-                        onChanged: (val) =>
-                            setState(() => _currentClassCode = val),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: RaisedButton(
-                        child: Text("Join"),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            setState(() {
-                              user.classCode =
-                                  _currentClassCode ?? user.classCode;
-                            });
-                            await _fireStoreUtils.updateCurrentUser(
-                                user, context);
-                            Navigator.pop(context);
-                          }
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
 
   _onCameraClick() {
     final action = CupertinoActionSheet(
