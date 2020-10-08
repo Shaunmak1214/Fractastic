adac9c1298905977bc143dbe4e391365318dd652
Min Xuan <63584286+minxuan1509@users.noreply.github.com>
2020-10-08 12:42:00 +0800

    game menu

--- lib/ui/chapter/chapter1/game1.dart
+++ lib/ui/chapter/chapter1/game1.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:simple_url_preview/simple_url_preview.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Chap1game1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: _launchURL,
-          child: Text('Play Game !'),
+        appBar: AppBar(
+          title: new Text('Welcome to Game Page'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          leading: new IconButton(
+            icon: new Icon(Icons.arrow_back),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
         ),
-      ),
-    );
+        body: new Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: SizedBox(height: 60.0),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: SimpleUrlPreview(
+                url:
+                    'https://www.mathplayground.com/fraction_forest_part1.html',
+                textColor: Colors.white,
+                bgColor: Colors.indigo[300],
+                isClosable: false,
+                titleLines: 2,
+                descriptionLines: 3,
+                imageLoaderColor: Colors.white,
+                previewHeight: 250,
+                previewContainerPadding: EdgeInsets.all(8.0),
+                onTap: () => print('This is Fraction game description'),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Center(
+                child: RaisedButton.icon(
+                  icon: Icon(Icons.link),
+                  color: Colors.blue[300],
+                  highlightColor: Colors.blue[300],
+                  textColor: Colors.black,
+                  onPressed: _launchURL,
+                  label: new Text('Play Game !'),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
 
